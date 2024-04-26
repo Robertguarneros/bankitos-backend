7f0a9f897d3d5b4a3821a7d57c12b583184b7452
Roberto <roberto.guarneros11@gmail.com>
2024-04-26 21:29:49 +0200

    Added mongodb config

--- lib/config/app.ts
+++ lib/config/app.ts
@@ -14,6 +14,8 @@ import { ConversationRoutes } from "../routes/conversation_routes";
 class App {
 
    public app: express.Application;
+   //if testing on local machine use the localhost link and comment the link below
+   //public mongoUrl: string = 'mongodb://localhost/' + environment.getDBName();
    public mongoUrl: string = 'mongodb://172.17.0.2:27017/' + environment.getDBName();
 
    private test_routes: TestRoutes = new TestRoutes();
