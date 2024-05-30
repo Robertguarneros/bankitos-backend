6fd4517eb57f1da3122e0139d3ec5ba532ba8fae
Roberto <roberto.guarneros11@gmail.com>
2024-05-30 18:47:54 +0200

    added socket io on port 3001

--- lib/config/app.ts
+++ lib/config/app.ts
@@ -2,6 +2,8 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as mongoose from 'mongoose';
 import * as cors from 'cors';
+import { createServer, Server as HttpServer } from 'http';
+import { Server as SocketIOServer } from 'socket.io';
 import environment from "../environment";
 import { TestRoutes } from "../routes/test_routes";
 import { UserRoutes } from "../routes/user_routes";
@@ -12,13 +14,11 @@ import { ConversationRoutes } from "../routes/conversation_routes";
 import { HousingRoutes } from "../routes/housing_routes";
 import { ChatbotRoutes } from "../routes/chatbot";
 
-
 class App {
-
    public app: express.Application;
-   //if testing on local machine use the localhost link and comment the link below
+   public httpServer: HttpServer;
+   public io: SocketIOServer;
    public mongoUrl: string = 'mongodb://localhost/' + environment.getDBName();
-   //public mongoUrl: string = 'mongodb://bankitos-mongodb:27017/' + environment.getDBName();
 
    private test_routes: TestRoutes = new TestRoutes();
    private common_routes: CommonRoutes = new CommonRoutes();
@@ -31,8 +31,32 @@ class App {
 
    constructor() {
       this.app = express();
+      this.httpServer = createServer(this.app);
+      this.io = new SocketIOServer(this.httpServer);
+
       this.config();
       this.mongoSetup();
+      this.setupRoutes();
+      this.setupSocketIO();
+   }
+
+   private config(): void {
+      this.app.use(bodyParser.json());
+      this.app.use(bodyParser.urlencoded({ extended: false }));
+      this.app.use(cors());
+   }
+
+   private mongoSetup(): void {
+      mongoose.connect(this.mongoUrl)
+         .then(() => {
+            console.log("MongoDB connected successfully.");
+         })
+         .catch((err) => {
+            console.error("MongoDB connection error:", err);
+         });
+   }
+
+   private setupRoutes(): void {
       this.test_routes.route(this.app);
       this.user_routes.route(this.app);
       this.place_routes.route(this.app);
@@ -40,29 +64,28 @@ class App {
       this.conversation_routes.route(this.app);
       this.housing_routes.route(this.app);
       this.chatbot_routes.route(this.app);
-      //Siempre dejarlo abajo del todo si no da error!!!
       this.common_routes.route(this.app);
-   
-      
    }
 
-   private config(): void {
-      // support application/json type post data
-      this.app.use(bodyParser.json());
-      //support application/x-www-form-urlencoded post data
-      this.app.use(bodyParser.urlencoded({ extended: false }));
-      // Enable CORS for all origins
-      this.app.use(cors());
+   private setupSocketIO(): void {
+      this.io.on('connection', (socket) => {
+         console.log('A user connected');
+         socket.emit('notification', { message: 'Welcome to the server!' });
+
+         socket.on('disconnect', () => {
+            console.log('A user disconnected');
+         });
+      });
    }
 
-   private mongoSetup(): void {
-      mongoose.connect(this.mongoUrl)
-          .then(() => {
-              console.log("MongoDB connected successfully.");
-          })
-          .catch((err) => {
-              console.error("MongoDB connection error:", err);
-          });
-  }
+   public startServer(): void {
+      this.httpServer.listen(3001, () => {
+         console.log(`Server is running on port ${3001}`);
+      });
+   }
 }
-export default new App().app;
+
+const appInstance = new App();
+appInstance.startServer();
+
+export default appInstance.app;
